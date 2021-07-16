b9d8f8f1f87c39134ed6217c200097c5defe6a2b
nermalneedscoffee <35051569+nermalneedscoffee@users.noreply.github.com>
2021-07-15 22:51:57 -0400

    responsive + cleanup
    i think it should work on mobile (i hope)

---
+++ jquery/home.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $(".course").click(function(){
+        $("html").addClass("overflow-hidden");
+    });
+    $(".popup-x").click(function(){
+        $("html").removeClass("overflow-hidden");
+    });
+    $("#nav-small").click(function(){
+        setTimeout(function(){
+            if ($("#nav-button").is(":checked")) {
+                $("html").addClass("overflow-hidden");
+            } else {
+                $("html").removeClass("overflow-hidden");
+            }
+        }, 50);
+    });
+    $(".nav-secondary-link").click(function(){
+        $("#nav-button").prop("checked", false);
+        $("html").removeClass("overflow-hidden");
+    });
+});
