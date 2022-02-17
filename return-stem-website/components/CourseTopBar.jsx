e3e89b3884be474614e62bda43626c25ef77cd28
Samuel Lihn <samuellihn@gmail.com>
2022-02-17 08:26:25 -0500

    doc system

---
+++ return-stem-website/components/CourseTopBar.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+import styles from "../styles/pages/guide.module.sass"
+
+export default function CourseTopBar(props) {
+    return (
+        <div className={styles["topbar"]}>
+            <p>hello world</p>
+
+        </div>
+    )
+}
+
+
--- return-stem-website/pages/_app.jsx
+++ return-stem-website/pages/_app.jsx
@@ -1,5 +1,6 @@
 import '../styles/base/normalize.css'
 import '../styles/base/globals.css'
+import "../styles/prism.css"
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
